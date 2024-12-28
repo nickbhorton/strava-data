636afb7ad90986217b82500c38e9cc9c43f8ebe4
Nick Horton <nickkbhorton@gmail.com>
2024-12-28 22:49:21 +0000

    sorting dates

--- src/dist_alt_file_creator.py
+++ src/dist_alt_file_creator.py
@@ -10,9 +10,28 @@ for fn in file_names:
     if fn.split(".")[1] == "json":
         ids.append(fn.split(".")[0])
 
-for id in ids:
-    print(id)
-    test_id = id
+act_discription_file = open("act2024/total.json", "r")
+act_disc_content = act_discription_file.read()
+act_json = json.loads(act_disc_content)
+
+date_id_pair = []
+
+for obj in act_json:
+    date_str = obj["start_date"]
+    month = date_str[5:7]
+    day = date_str[8:10]
+    sort_val = (int(month) * 40) + int(day)
+    id = obj["id"]
+    if str(id) in ids:
+        date_id_pair.append([sort_val, id])
+
+date_id_pair.sort()
+# print(date_id_pair)
+
+fail_count = 0
+for index in range(len(date_id_pair)):
+    print(date_id_pair[index][1])
+    test_id = str(date_id_pair[index][1])
 
     json_file = open(data_root + test_id + ".json", "r")
     content = json_file.read()
@@ -20,18 +39,51 @@ for id in ids:
 
     json_content = json.loads(content)
     if json_content.get("distance") == None:
+        fail_count += 1
         continue
     if json_content.get("altitude") == None:
+        fail_count += 1
         continue
     distance = json_content["distance"]["data"]
     altitude = json_content["altitude"]["data"]
 
-    out_file = open(data_send_root + str(id) + ".data", "w")
+    new_dist = []
+    new_alt = []
+    avg_window = 10
+    for i in range(len(distance) // avg_window):
+        avg_dist = 0
+        avg_alt = 0
+        for j in range(avg_window):
+            avg_dist += distance[i * avg_window + j]
+            avg_alt += altitude[i * avg_window + j]
+        avg_dist /= avg_window
+        avg_alt /= avg_window
+        new_dist.append(avg_dist)
+        new_alt.append(avg_alt)
 
-    for i in range(len(altitude)):
-        out_file.write(str(distance[i]))
+    last_avg_dist = 0
+    last_avg_alt = 0
+    for i in range(len(distance) % avg_window):
+        last_avg_dist += distance[len(distance) - 1 - i]
+        last_avg_alt  += altitude[len(distance) - 1 - i]
+
+    if (len(distance) % avg_window != 0):
+        last_avg_dist /= len(distance) % avg_window
+        last_avg_alt /= len(distance) % avg_window
+
+        new_dist.append(last_avg_dist)
+        new_alt.append(last_avg_alt)
+
+    first_alt = new_alt[0]
+    for i in range(len(new_alt)):
+        new_alt[i] -= first_alt
+
+    out_file = open(data_send_root + str(index - fail_count) + ".data", "w")
+
+    for i in range(len(new_dist)):
+        out_file.write(str(new_dist[i]))
         out_file.write(" ")
-        out_file.write(str(altitude[i]))
+        out_file.write(str(new_alt[i]))
         out_file.write("\n")
 
     out_file.close()
--- src/dist_alt_visualizer.py
+++ src/dist_alt_visualizer.py
@@ -11,6 +11,7 @@ for fn in file_names:
         ids.append(fn.split(".")[0])
 
 test_id = ids[202]
+print(test_id)
 
 json_file = open(data_root + test_id + ".json", "r")
 content = json_file.read()
@@ -27,4 +28,43 @@ for i in range(len(altitude)):
 plt.plot(distance, altitude)
 plt.xlabel("distance (miles)")
 plt.ylabel("altitude (feet)")
+
+
+new_dist = []
+new_alt = []
+avg_window = 10
+for i in range(len(distance) // avg_window):
+    avg_dist = 0
+    avg_alt = 0
+    for j in range(avg_window):
+        avg_dist += distance[i * avg_window + j]
+        avg_alt += altitude[i * avg_window + j]
+    avg_dist /= avg_window
+    avg_alt /= avg_window
+    new_dist.append(avg_dist)
+    new_alt.append(avg_alt)
+
+last_avg_dist = 0
+last_avg_alt = 0
+for i in range(len(distance) % avg_window):
+    last_avg_dist += distance[len(distance) - 1 - i]
+    last_avg_alt  += altitude[len(distance) - 1 - i]
+
+last_avg_dist /= len(distance) % avg_window
+last_avg_alt /= len(distance) % avg_window
+
+new_dist.append(last_avg_dist)
+new_alt.append(last_avg_alt)
+
+plt.plot(new_dist, new_alt)
+
+first_alt = new_alt[0]
+for i in range(len(new_alt)):
+    new_alt[i] -= first_alt
+
+plt.plot(new_dist, new_alt)
+
+
 plt.show()
+
+
